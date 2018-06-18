2f1cdd8959bf7c7478bfefb4f436eff7e5ea7e05
KamAbzalov <ok_kam90@mail.ru>
2018-06-18 18:55:30 +0000

    route.php ?????????????? ?????? ?? Bitbucket

--- conf/route.php
+++ conf/route.php
@@ -22,29 +22,31 @@ class Routing {
         $i = count($route)-1;
 
         while($i>0) {
-            if($route[$i] != '') {
-                if(is_file(CONTROLLER_PATH . ucfirst($route[$i]) . "Controller.php")) {
-                    $controllerName = ucfirst($route[$i]) . "Controller";
-                    $modelName =  ucfirst($route[$i]) . "Model";
-                    break;
-                } else {
-                     $action = $route[$i];
+                if($route[$i] != '') {
+                    if(is_file(CONTROLLER_PATH . ucfirst($route[$i]) . "Controller.php")) {
+                        $controllerName = ucfirst($route[$i]) . "Controller";
+                        $modelName =  ucfirst($route[$i]) . "Model";
+                        break;
+                    } else {
+                         $action = $route[$i];
+                    }
                 }
+                $i--;
             }
-            $i--;
-        }
-
-        require_once CONTROLLER_PATH . $controllerName . ".php";
-        require_once MODEL_PATH . $modelName . ".php";
-
-        $controller = new $controllerName();
-        $controller->$action();
-    
-
+            require_once CONTROLLER_PATH . $controllerName . ".php";
+            require_once MODEL_PATH . $modelName . ".php";
+            
+            $controller = new $controllerName();
+            
+            if(method_exists($controller, $action)) {
+                $controller->$action();
+            } else {
+                self::errorPage();
+            }
     }
 
-    public function errorPage() {
-
+    public static function errorPage() {
+        header("Location: /404.php");
     }
 
 }
