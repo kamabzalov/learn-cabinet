bbdd9402154788a8feafa40f3a1caf306c0181fa
KamAbzalov <ok_kam90@mail.ru>
2019-01-29 18:34:34 +0000

    route.php ?????????????? ?????? ?? Bitbucket

--- conf/route.php
+++ conf/route.php
@@ -17,20 +17,22 @@ class Routing {
         $controllerName = "IndexController";
         $modelName = "IndexModel";
         $action = "index";
-
         $route = explode("/", parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
         $i = count($route)-1;
 
         while($i>0) {
-                if($route[$i] != '') {
-                    if(is_file(CONTROLLER_PATH . ucfirst($route[$i]) . "Controller.php")) {
-                        $controllerName = ucfirst($route[$i]) . "Controller";
-                        $modelName =  ucfirst($route[$i]) . "Model";
-                        break;
-                    } else {
-                         $action = $route[$i];
-                    }
+            if($route[1] != 'cabinet') {
+                header('Location: /cabinet');
+            }
+            if($route[$i] != '') {
+                if(is_file(CONTROLLER_PATH . ucfirst($route[$i]) . "Controller.php")) {
+                    $controllerName = ucfirst($route[$i]) . "Controller";
+                    $modelName =  ucfirst($route[$i]) . "Model";
+                    break;
+                } else {
+                    $action = $route[$i];
                 }
+            }
                 $i--;
             }
             require_once CONTROLLER_PATH . $controllerName . ".php";
